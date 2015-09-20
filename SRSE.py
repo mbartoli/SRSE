20973a0225af38ff79518ca9b1bf0db2b87fcbd9
mbartoli <bartolimichael@gmail.com>
2015-09-19 17:44:30 -0700

    Implementation of the Huerta reading ease formula

--- SRSE.py
+++ SRSE.py
@@ -8,7 +8,7 @@ import time
 import pprint
 import sys
 import nltk.data
-
+import math
 
 def countSyllables(word): 
     """
@@ -37,6 +37,17 @@ def countSyllables(word):
 
 
 def scoreHuerta(text):
+    """
+    The Huerta Reading Ease is a Modified Flesch Reading Ease 
+    for Spanish Texts.
+
+    Scores run roughly from 30 to 100, with higher scores being easier to read.
+    
+    Huerta Reading Ease = 206.84 - (0.60 * P) - (1.02 * F)
+    (Using 100 word chunks of text)
+    P = Number of syllables per 100 words
+    F = Number of sentences per 100 words
+    """
     tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
     sample = tokenizer.tokenize(text.decode("utf8"), text)
     totalSentences = len(sample)
@@ -49,6 +60,18 @@ def scoreHuerta(text):
         for j in range(0, numOfWords):
             totalSyllables = totalSyllables + countSyllables(sentence[j])
         totalWords = totalWords + numOfWords
+
+    # avg number of syllabes per 100 words 
+    buckets = float(totalWords) / 100
+    #print "totalSyllables " + str(totalSyllables) 
+    #print "totalWords " + str(totalWords)
+    #print "totalSents " + str(totalSentences)
+    P = (float(totalSyllables) / totalWords) * 100
+    F = (float(totalSentences) / totalWords) * 100
+
+    # Huerta Reading Ease = 206.84 - (0.60 * P) - (1.02 * F)
+    score = 206.84 - (0.60 * P) - (1.02 * F) 
+    return score
 
 
 def main():
